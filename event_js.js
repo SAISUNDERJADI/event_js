145ae3b2e87bb363132c3f8a200d95f16a2babfb
saisunderjadi <saisunderjadi@gmail.com>
2024-11-03 23:24:21 +0530

    event handling in js

---
+++ event_js.js
@@ -0,0 +1,59 @@
+
+let hov=document.getElementById("hov").style.marginLeft="50px";
+
+let a=document.getElementById("a1").style.textDecoration="none";
+
+function cl(){
+    let p=document.getElementById("p1")
+    p.innerHTML="<b style='color:red;font-size:30px'>Hii this is the hidden paragraph content</b>"
+}
+function cll(){
+    let p=document.getElementById("p1")
+    p.innerHTML=""
+}
+
+
+function opacity1(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0'>"
+}
+function opacity2(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.1'>"
+}
+function opacity3(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.2'>"
+}
+function opacity4(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.3'>"
+}
+function opacity5(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.4'>"
+}
+function opacity6(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.5'>"
+}
+function opacity7(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.6'>"
+}
+function opacity8(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.7'>"
+}
+function opacity9(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.8'>"
+}
+function opacity10(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:0.9'>"
+}
+function opacity11(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='js.jpg' style='opacity:1'>"
+}
